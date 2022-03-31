4d12be3787c00188350fafd1749e527a0005d914
rahathossain3 <rahathossain210@gmail.com>
2022-03-31 21:36:33 +0600

    search a mea by name

---
+++ src/components/Meal/Meal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Meal.css'
+
+const Meal = (props) => {
+    const { strMeal, strInstructions, strMealThumb } = props.meal;
+    const { handleAddToOrder, meal } = props;
+
+    return (
+        <div className='meal'>
+            <img src={strMealThumb} alt="" />
+            <h4>{strMeal}</h4>
+            <p>{strInstructions.slice(0, 100)}</p>
+            <button onClick={() => handleAddToOrder(meal)}> Add This Food</button>
+
+        </div>
+    );
+};
+
+export default Meal;
